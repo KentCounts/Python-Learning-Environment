32d046ee957d25976b7e989f848f95e97206bab7
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-04-16 10:00:33 -0500

    finished batch renamer and hooked into main menu

--- Python Tools/main.py
+++ Python Tools/main.py
@@ -2,20 +2,29 @@ import os
 import shutil
 import argparse
 import file_organizer
+import batch_renamer
 
 def main():
-    print("Automation Tools")
-    print("1. File Organizer")
-    print("2. Exit")
+    while True:
+        print("\nAutomation Tools")
+        print("1. File Organizer")
+        print("2. Batch Renamer")
+        print("3. Exit")
 
-    choice = input("Select an option: ").strip()
+        choice = input("Select an option: ").strip()
 
-    if choice == "1":
-        file_organizer.run_interactive()
-    elif choice == "2":
-        print("Exiting.")
-    else:
-        print("Invalid choice.")
+        if choice == "1":
+            file_organizer.run_interactive()
+
+        elif choice == "2":
+            batch_renamer.run_interactive()
+
+        elif choice == "3":
+            print("Exiting.")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
 
 
 if __name__ == "__main__":
